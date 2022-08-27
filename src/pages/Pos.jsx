017da7729a475ec79286a3e44fe8abaac65b2890
Wasim <waseem_wmgh@yahoo.com>
2022-08-27 17:03:57 +0200

    logout button

--- src/pages/Pos.jsx
+++ src/pages/Pos.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 import Main from "../Main";
-
+import "./Pos.css";
 const Pos = () => {
   const { user, logout } = UserAuth();
   const navigate = useNavigate();
@@ -21,7 +21,7 @@ const Pos = () => {
     <>
       <Main />
       <div className="max-w-[600px] mx-auto my-16 p-4">
-        <button onClick={handleLogout} className="border px-6 py-2 my-4">
+        <button onClick={handleLogout} className="border px-6 py-2 my-4 logout">
           Logout
         </button>
         <h1 className="text-2xl font-bold py-4">Pos</h1>
